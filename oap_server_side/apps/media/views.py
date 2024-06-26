2aa39310f1ac1fd99ec782327ad25c28021cdc8a
Douae-Kbl <kabelma.douae@gmail.com>
2024-06-26 13:02:09 +0100

    added restrictions on video and playlist owners

--- oap_server_side/apps/media/views.py
+++ oap_server_side/apps/media/views.py
@@ -1,5 +1,5 @@
 from ..users.permissions import IsAdmin
-
+from .permissions import IsOwnerOrReadOnly
 from rest_framework import viewsets
 from .models import Category, Media, Comment, View
 from .serializers import CategorySerializer, MediaSerializer, CommentSerializer, ViewSerializer
@@ -7,16 +7,15 @@ from .serializers import CategorySerializer, MediaSerializer, CommentSerializer,
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAdmin]
+    permission_classes = [IsAdmin,IsOwnerOrReadOnly]
 
 
 class MediaViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAdmin]
+    permission_classes = [IsAdmin,IsOwnerOrReadOnly]
     queryset = Media.objects.all()
     serializer_class = MediaSerializer
     
-
-
+    
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
