f2c2adb3ca75e0089c8ea8df78fbe288f6dbed10
Fatima El Kabir <fatima.elkabir2002@gmail.com>
2024-07-05 22:01:28 +0100

    url

--- oap_server_side/apps/users/urls.py
+++ oap_server_side/apps/users/urls.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('register/', UserRegister.as_view(), name='register'),
     path('login/', MyTokenObtainPairView.as_view(), name='login'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('verify/', VerifyTokenView.as_view(), name='token_verify'),
     path('test/', TestView.as_view(), name='test'),
     path('role/', UserRoleView.as_view(), name='role'),
