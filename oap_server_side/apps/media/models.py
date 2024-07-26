7ada11ab4b5c3d58126c58fd7f2e1504282957b1
yassib1 <86356206+yibork@users.noreply.github.com>
2024-07-26 22:05:07 +0100

    fix media seriallizer

--- oap_server_side/apps/media/models.py
+++ oap_server_side/apps/media/models.py
@@ -3,7 +3,7 @@ from apps.users.models import User
 from django.utils import timezone
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,unique=True)
     def __str__(self):
         return self.name
 
--- oap_server_side/apps/media/services.py
+++ oap_server_side/apps/media/services.py
@@ -1,23 +1,26 @@
 from .serializers import MediaSerializer, CategorySerializer
 
+from .models import Category, Media
+from .serializers import CategorySerializer, MediaSerializer
+
 def create_media_with_category(media_data, category_data=None):
-    #get category and check if it is dictionary, if it is then create new category 
-
+    # Get or create category
     if isinstance(category_data, dict):
-        category_serializer = CategorySerializer(data=category_data)
-        if category_serializer.is_valid():
-            category = category_serializer.save()
-            media_data['categoryID'] = category.id
-        else:
-            return None, category_serializer.errors
-    
-    print("idk",media_data)
+        category_name = category_data.get('name')
+        category, created = Category.objects.get_or_create(name=category_name)
+        media_data['categoryID'] = category.id
+    elif isinstance(category_data, str):  # Assuming category_data can also be a string for category name
+        category, created = Category.objects.get_or_create(name=category_data)
+        media_data['categoryID'] = category.id
+    else:
+        return None, "Invalid category data provided"
 
+    # Debugging print statement
+    print("Media Data:", media_data)
+
+    # Create media
     media_serializer = MediaSerializer(data=media_data)
     if media_serializer.is_valid():
         media = media_serializer.save()
         return media, None
     return None, media_serializer.errors
-
-
-    
